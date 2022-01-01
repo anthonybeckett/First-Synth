3cf31edf75a1f8729e73fd24a6fafa9f1fdbc467
Anthony Beckett <antneyb@hotmail.co.uk>
2022-01-01 20:19:08 +0000

    Fixed issues with clicks and pops creating a new buffer and adding the processing from the new buffer to the existing one

--- Source/SynthVoice.cpp
+++ Source/SynthVoice.cpp
@@ -24,6 +24,9 @@ void SynthVoice::startNote(int midiNoteNumber, float velocity, juce::Synthesiser
 void SynthVoice::stopNote(float velocity, bool allowTailOff)
 {
     adsr.noteOff();
+
+    if (!allowTailOff || !adsr.isActive())
+        clearCurrentNote();
 }
 
 void SynthVoice::pitchWheelMoved(int newPitchWheelValue)
@@ -48,6 +51,12 @@ void SynthVoice::prepareToPlay(double sampleRate, int samplesPerBlock, int outpu
 
     gain.setGainLinear(0.5f);
 
+    adsrParams.attack = 0.8f;
+    adsrParams.decay = 0.8f;
+    adsrParams.sustain = 1.f;
+    adsrParams.release = 1.5f;
+    adsr.setParameters(adsrParams);
+
     isPrepared = true;
 }
 
@@ -55,9 +64,23 @@ void SynthVoice::renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int sta
 {
     jassert(isPrepared);
 
-    juce::dsp::AudioBlock<float> audioBlock { outputBuffer };
+    if (!isVoiceActive())
+        return;
+
+    synthBuffer.setSize(outputBuffer.getNumChannels(), numSamples, false, false, true);
+    synthBuffer.clear();
+
+    juce::dsp::AudioBlock<float> audioBlock { synthBuffer };
     osc.process(juce::dsp::ProcessContextReplacing<float>(audioBlock));
     gain.process(juce::dsp::ProcessContextReplacing<float>(audioBlock));
 
-    adsr.applyEnvelopeToBuffer(outputBuffer, startSample, numSamples);
+    adsr.applyEnvelopeToBuffer(synthBuffer, 0, synthBuffer.getNumSamples());
+
+    for (int channel = 0; channel < outputBuffer.getNumChannels(); ++channel)
+    {
+        outputBuffer.addFrom(channel, startSample, synthBuffer, channel, 0, numSamples);
+
+        if (!adsr.isActive())
+            clearCurrentNote();
+    }
 }
